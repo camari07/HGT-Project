167ddc4d40a85490a430f1d0f3ee1975e6b8dec7
Frederick Otu-Afro <frederickotuafro@gmail.com>
2026-05-21 11:00:02 +0000

    Fix home route and header link

--- Logtracker/src/components/header.jsx
+++ Logtracker/src/components/header.jsx
@@ -7,7 +7,7 @@ function Header({ isloggedIn, setIsLoggedIn, navigate }) {
         <header className="fixed top-0 w-full bg-gray-800 text-white p-4 flex justify-between items-center z-50">
             <nav>
                 <ul className="flex space-x-6">
-                    <li><Link to="/" className="hover:text-green-400">Home</Link></li>
+                    <li><Link to="/home" className="hover:text-green-400">Home</Link></li>
                     <li><Link to="/about" className="hover:text-green-400">About</Link></li>
                     <li><Link to="/contact" className="hover:text-green-400">Contact</Link></li>
                 </ul>
