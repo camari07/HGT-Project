997463b7c82fb7f98c21bce9dcdaf48423aac7ff
camari07 <yslsg170@gmail.com>
2026-05-21 08:07:54 +0000

    fix: update ALLOWED_HOSTS to include production domain

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-&&anunxfufof+gj#%0hfm5)po0qa)&o9m##a0r1$%2s9%9#2w9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
