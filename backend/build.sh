c55059256974c50955d9a2ad7f4f8be482a94a74
Frederick Otu-Afro <frederickotuafro@gmail.com>
2026-05-22 14:04:47 +0000

    Fix database settings config to read environment variables

--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-
-# 1. Clear out the migration history tracker specifically for your app
-python manage.py migrate posts zero --fake
-
-# 2. Re-apply everything with a hard sync to build the raw tables
-python manage.py migrate --run-syncdb
+python manage.py migrate
