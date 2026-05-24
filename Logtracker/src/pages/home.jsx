b0153e96606949c281a10f07f29f35c7df4f1c44
camari07 <yslsg170@gmail.com>
2026-05-24 17:43:26 +0000

    refactor: update Home component layout and button styles

--- Logtracker/src/pages/home.jsx
+++ Logtracker/src/pages/home.jsx
@@ -1,17 +1,35 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-        <div className="grid place-items-center h-screen">
-            <div className="grid justify-items-center bg-green-100 p-10 rounded-lg">
-                <h1>Welcome to Holland Greentech LogTracker</h1>
-                <p>Track your farm activities, irrigation, and maintenance logs all in one place.</p>
-                <Link to="/irrigation">
-                    <button className="bg-green-500 p-2 text-white rounded-md mt-4">Go to Irrigation Logs</button>
-                </Link>
+        return(
+        <div class="flex flex-col md:flex-row jusify-center md:justify-between items-center mt-10 bg-green-100 h-150 p-5">
+            <div class="mr-5">
+
+                    <img src="https://picsum.photos/536/354" alt="lorem ipsum" />
+                    <Link to="/irrigation">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 mt-2 rounded">Record Irrigation Activity</button>
+                    </Link>
+      
             </div>
+            <div class="mr-5">
+                    <img src="https://picsum.photos/536/354" alt="lorem ipsum" />
+                    <Link to="/farm">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 mt-2 rounded">Record Farm Activity</button>
+                    </Link>
+            </div>
+            <div class="mr-5">
+                    <img src="https://picsum.photos/536/354" alt="lorem ipsum" />
+                    <Link to="/maintenance">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 mt-2 rounded">Record Maintenance Activity</button>
+                    </Link>
+            </div>
+            <div class="mr-5">
+                    <img src="https://picsum.photos/536/354" alt="lorem ipsum" />
+                    <h3>Report a problem</h3>
+            </div>
+
         </div>
-    )
-}
+)}
 export default Home;
