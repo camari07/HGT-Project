99d3c3c75778ac37a7b5363a894f2ed89452f72d
camari07 <yslsg170@gmail.com>
2026-05-24 22:29:24 +0000

    feat: enhance Home component layout with new welcome section and image

--- Logtracker/src/pages/home.jsx
+++ Logtracker/src/pages/home.jsx
@@ -4,37 +4,46 @@ import { Link } from "react-router-dom";
 import irrigationImg from "../assets/irrigate.jpg";
 import farmImg from "../assets/farming.jpg";
 import maintenanceImg from "../assets/farmact.jpeg";
+import images from "../assets/images.jpeg";
 
 function Home() {
         return(
-        <div className="flex flex-col md:flex-row justify-center items-center md:justify-between mt-10 bg-white h-150">
-            <div className="md:mr-5 md:w-1/4 flex flex-col items-center md:justify-between">
+         <div>  
+            <div className="flex">
+                
+                <img className="mt-15  h-15 md:h-24 w-24" src={images} alt="hgt_logo" />
+                <h2 className="bg-red-100 pd-20">Welcome to the Home Page</h2>
+            </div>
+            <div className="flex flex-col md:flex-row justify-center md:justify-between mt-10 bg-white h-150">
+                
+                <div className="md:mr-5 md:w-1/4 flex flex-col items-center md:justify-between">
 
-                    <img className="ml-4 mt-30 max-h-30 md:max-h-auto rounded object-contain " src={irrigationImg} alt="lorem ipsum" />
-                    <Link to="/irrigation">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white ml-4 mt-2 rounded w-40">Irrigation Activity</button>
-                    </Link>
-      
-            </div>
-            <div className="md:mr-5 mt-2 md:w-1/4 flex flex-col items-center md:justify-between">
-                    <img className="ml-4 max-h-30 md:max-h-auto object-contain " src={farmImg} alt="lorem ipsum" />
-                    <Link to="/farm">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white ml-4 mt-2 rounded w-40">Record Farm Activity</button>
-                    </Link>
-            </div>
-            <div className="md:mr-5 md:w-1/4 flex flex-col items-center md:justify-between">
-                    <img className="ml-4 mt-2 max-h-30 md:max-h-auto object-contain " src={maintenanceImg} alt="lorem ipsum" />
-                    <Link to="/maintenance">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white ml-4 mt-2 rounded w-40">Maintenance Activity</button>
-                    </Link>
-            </div>
-            <div className="md:mr-5 md:w-1/4 flex flex-col items-center md:justify-between"> 
-                    <img className="ml-4 mt-2 max-h-30 md:max-h-auto object-contain " src="https://picsum.photos/536/354" alt="lorem ipsum" />
-                    <Link to="/maintenance">
-                        <button className="bg-green-500 hover:bg-green-700 text-white ml-4 mt-2 rounded w-40">Report a problem</button>
-                    </Link>
-            </div>
+                        <img className="ml-4 mt-0 md:mt-2 max-h-30 md:max-h-auto rounded object-contain " src={irrigationImg} alt="lorem ipsum" />
+                        <Link className="md:mb-100" to="/irrigation">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white ml-4 mt-2 rounded w-40">Irrigation Activity</button>
+                        </Link>
+        
+                </div>
+                <div className="md:mr-5 mt-2 md:w-1/4 flex flex-col items-center md:justify-between">
+                        <img className="ml-4 max-h-30 md:max-h-auto object-contain " src={farmImg} alt="lorem ipsum" />
+                        <Link className="md:mb-100" to="/farm">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white ml-4 mt-2 rounded w-40">Record Farm Activity</button>
+                        </Link>
+                </div>
+                <div className="md:mr-5 md:w-1/4 flex flex-col items-center md:justify-between">
+                        <img className="ml-4 mt-2 max-h-30 md:max-h-auto object-contain " src={maintenanceImg} alt="lorem ipsum" />
+                        <Link className="md:mb-100" to="/maintenance">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white ml-4 mt-2 rounded w-40">Maintenance Activity</button>
+                        </Link>
+                </div>
+                <div className="md:mr-5 md:w-1/4 flex flex-col items-center md:justify-between"> 
+                        <img className="ml-4 mt-2 max-h-30 md:max-h-auto object-contain " src="https://picsum.photos/536/354" alt="lorem ipsum" />
+                        <Link className="md:mb-100" to="/maintenance">
+                            <button className="bg-green-500 hover:bg-green-700 text-white ml-4 mt-2 rounded w-40">Report a problem</button>
+                        </Link>
+                </div>
 
-        </div>
+            </div>
+        </div> 
 )}
 export default Home;
